d2a50065670de5132c5a2ebb0f4a3a650b3076cb
snandasena <sajithgithub@gmail.com>
2024-02-14 13:55:27 +0100

    Improved

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -77,13 +77,33 @@ pub mod methods {
 }
 
 pub mod closure {
-   pub fn test_closure() {
+    pub fn test_closure() {
         let outer_var = 42;
 
-        let closure_annotated = |i: i32 | -> i32{ i + outer_var };
-        let closure_inferred = |i|      i + outer_var;
+        let closure_annotated = |i: i32| -> i32{ i + outer_var };
+        let closure_inferred = |i| i + outer_var;
 
         println!("closure_annotated: {}", closure_annotated(1));
         println!("closure_inferred: {}", closure_inferred(1));
+    }
+
+    fn apply<F>(f: F) where
+        F: FnOnce()
+    {
+        f();
+    }
+
+    fn apply_to_3<F>(f: F) -> i32 where F: Fn(i32) -> i32 {
+        f(3)
+    }
+
+    fn test_closure_as_input_params()
+    {
+        use std::mem;
+
+        let greeting = "hello";
+        let mut farewell = "goodbye".to_owned();
+
+
     }
 }
