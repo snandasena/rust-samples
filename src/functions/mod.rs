6393bc24513488276e0cb75952999736ed2a6d83
snandasena <sajithgithub@gmail.com>
2024-02-14 16:34:26 +0100

    Improved

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -9,7 +9,9 @@ pub fn test_methods()
 pub fn test_closures()
 {
     captures::test_closure();
-    captures::test_closure_as_input_params()
+    captures::test_closure_as_input_params();
+    captures::test_capture();
+    captures::test_move();
 }
 
 
